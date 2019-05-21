ab3250af79d47009f9d6e1769504cfe4a6c3bafc
Renê Lopes <renelopes.over@gmail.com>
2019-05-20 22:36:40 -0300

    Orientação a Objeto

--- aulas/orientação a objeto/retangulo4.py
+++ aulas/orientação a objeto/retangulo4.py
@@ -1,7 +1,5 @@
 # coding: utf-8
 
-#MÉTODOS ASSESSORES
-
 class Retangulo:
 
     def __init__(self, largura, altura):
@@ -10,15 +8,16 @@ class Retangulo:
         self.set_altura(altura)
         self.set_largura(largura)
     def set_altura(self, num):
-        if(not(isinstance(num, int)and(num>0))):
+        if(not(isinstance(num, int)and(num > 0))):
             raise ValueError("Altura é inválida: {}".format(num))
         self.altura = num
     def set_largura(self, num):
-        if(not(isinstance(num, int)and(num>0))):
+        if (not (isinstance(num, int) and (num > 0))):
             raise ValueError("Largura é inválida: {}".format(num))
         self.largura = num
     def get_area(self):
         return self.altura * self.largura
 
-r = Retangulo(largura=10, altura=5)
-# r = Retangulo(largura=10, altura='-1')
+
+# r = Retangulo(largura=10, altura=5)
+r = Retangulo(largura=5, altura="-1")
--- aulas/orientação a objeto/retangulo_property.py
+++ aulas/orientação a objeto/retangulo_property.py
@@ -1,27 +1,41 @@
 # coding: utf-8
 
-# CLASSE RETANGULO COM PROPERTY
+# CLASSE RETANGULO COM PROPERTY 2
 
 class Retangulo:
 
     def __init__(self, largura, altura):
         self._largura = 0
         self._altura = 0
-        self.__var = 0
-        self.set_altura(altura)
-        self.set_largura(largura)
+        self.altura = altura
+        self.largura = largura
+    @property
+    def altura(self):
+        return self._altura
     @altura.setter
     def altura(self, num):
         if(not(isinstance(num, int)and(num>0))):
             raise ValueError("Altura é inválida: {}".format(num))
         self._altura = num
-        self.__var = 456
-    def set_largura(self, num):
+    @property
+    def largura(self):
+        return self._largura
+    @largura.setter
+    def largura(self, num):
         if(not(isinstance(num, int)and(num>0))):
             raise ValueError("Largura é inválida: {}".format(num))
         self._largura = num
-    def get_area(self):
+    @property
+    def _get_area(self):
         return self._altura * self._largura
 
-r = Retangulo(largura=10, altura=5)
-r = Retangulo(largura=10, altura=5)
+    # altura = property(fget=_get_altura, fset=_set_altura)
+    # largura = property(fget=_get_largura, fset=_set_largura)
+    # area = property(fget=_get_area)
+
+r = Retangulo(largura=5, altura=5)
+r.largura = 10
+r.altura = 15
+print(r.altura)
+print(r.largura)
+print()
